18b1c23833bb98d592923dfde5dd93d8ba0e7068
AggressivelyMeows <cerulean.connor@gmail.com>
2020-01-18 19:23:16 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# First we need to install all the requirements
-sudo apt-get install docker.io
-docker run -it --rm openapitools/openapi-generator-cli generate -i ./reference/oa3-sponsus.yml -g AsciiDoc -o ./public
+wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/4.2.2/openapi-generator-cli-4.2.2.jar -O openapi-generator-cli.jar
+
+java -jar openapi-generator-cli.jar generate -i ./reference/oa3-sponsus.yml -g AsciiDoc -o ./public
