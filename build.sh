932f995897faecea1b15887ca6f04bd078b09a30
AggressivelyMeows <cerulean.connor@gmail.com>
2020-01-18 18:47:38 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 
 # First we need to install all the requirements
-npm install -g swagger-to-slate
-gem install bundler
-
-# widdershins allows us to convert OA3 to Markdown for use in Slate
-# but we dont need it for now so we wait until slate is installed
-git clone https://github.com/cerutech/sponsus-docs.git slate
-# The slate install will be located at ./slate
-# Ok cool, now that its all installed now we gotta generate our Markdown.
-# To inject our Markdown however, we need to overwrite the old Markdown file with our new one.
-# widdershins --search true --expandBody true -v true -r true --language_tabs 'ruby:Ruby' 'python:Python' 'shell:Shell' --summary reference/oa3-sponsus.yml -o ./slate/source/index.html.md
-swagger-to-slate -i /opt/buildhome/reference/oa3-sponsus.yml -o /opt/buildhome/slate/source/index.html.md
-
-cd ./slate
-bundle install
-bundle exec middleman build --clean
+docker run -it --rm openapitools/openapi-generator-cli generate -i ./reference/oa3-sponsus.yml -g AsciiDoc -o ./public
