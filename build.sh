096703eb5f85fb0fbabcf06f961905749e0b3cd1
AggressivelyMeows <cerulean.connor@gmail.com>
2020-01-18 16:03:52 +0000

    Forgot to add bundle install to the tasks

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 # First we need to install all the requirements
 npm install -g widdershins
 gem install bundler
+
 # widdershins allows us to convert OA3 to Markdown for use in Slate
 # but we dont need it for now so we wait until slate is installed
 git clone https://github.com/cerutech/sponsus-docs.git slate
@@ -12,4 +13,5 @@ git clone https://github.com/cerutech/sponsus-docs.git slate
 widdershins --search false --language_tabs 'ruby:Ruby' 'python:Python' --summary reference/oa3-sponsus.yml -o ./slate/source/index.html.md
 
 cd ./slate
+bundle install
 bundle exec middleman build --clean
