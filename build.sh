f0a76e8e2b6df20bb1296e89bf0d978a956c0110
AggressivelyMeows <cerulean.connor@gmail.com>
2020-01-18 16:10:41 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ git clone https://github.com/cerutech/sponsus-docs.git slate
 # The slate install will be located at ./slate
 # Ok cool, now that its all installed now we gotta generate our Markdown.
 # To inject our Markdown however, we need to overwrite the old Markdown file with our new one.
-widdershins --search false --language_tabs 'ruby:Ruby' 'python:Python' --summary reference/oa3-sponsus.yml -o ./slate/source/index.html.md
+widdershins --search true --expandBody true -v true -r true --language_tabs 'ruby:Ruby' 'python:Python' 'shell:Shell' --summary reference/oa3-sponsus.yml -o ./slate/source/index.html.md
 
 cd ./slate
 bundle install
