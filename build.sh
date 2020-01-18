631cd2fd9aa725aa922c35fedefd5a96c22053dc
AggressivelyMeows <cerulean.connor@gmail.com>
2020-01-18 18:55:20 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # First we need to install all the requirements
-apt-get install docker.io
+sudo apt-get install docker.io
 docker run -it --rm openapitools/openapi-generator-cli generate -i ./reference/oa3-sponsus.yml -g AsciiDoc -o ./public
