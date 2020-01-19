01dbfb7a19a301f7bdfd9eeea45fdbf3991cdd05
AggressivelyMeows <cerulean.connor@gmail.com>
2020-01-19 02:15:20 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-npm install @openapitools/openapi-generator-cli -g
-openapi-generator version
+mkdir public
+mv ./public.html ./public/index.html
 
-openapi-generator generate -i ./reference/oa3-sponsus.yml --skip-validate-spec -g html2 -o ./public
+#npm install @openapitools/openapi-generator-cli -g
+#openapi-generator version
+
+#openapi-generator generate -i ./reference/oa3-sponsus.yml --skip-validate-spec -g html2 -o ./public
